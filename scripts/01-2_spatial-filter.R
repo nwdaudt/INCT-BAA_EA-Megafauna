6f9993ea9025a09bb7930d3176aa44abd981d783
nwdaudt <nicholaswdaudt@gmail.com>
2025-09-15 15:27:05 +1100

    cleaned unused lines in the script

--- scripts/01-2_spatial-filter.R
+++ scripts/01-2_spatial-filter.R
@@ -6,11 +6,9 @@
 ### ----- [NO NEED TO RUN THIS SCRIPT AGAIN]
 
 ## Libraries ####
-# library(plyr)
 library(dplyr)
 library(sf)
 library(mapview)
-library(rnaturalearth)
 
 ## Read data ####
 
@@ -70,7 +68,7 @@ sf_mammals_spp <-
                 ! is.na(lon)) %>%
   sf::st_as_sf(coords = c("lon", "lat"), crs = 4326)
 
-## Spatial filter (only data inside Brazil+Blue Amazon) ####
+## Spatial filter (only data inside Brazil+Blue Amazon), and save them ####
 
 ## Create a polygon to identify records outside Brazil+Blue Amazon
 sf::sf_use_s2(FALSE)
@@ -125,85 +123,39 @@ rm(not_inside_birds, not_inside_mammals, not_inside_turtles,
    NA_birds_spp, NA_mammals_spp, NA_turtles_spp,
    sf_BRA_plus_BA)
 
-## [CHECK WITH COORDINATORS] Create polygon BR to crop records on land ####
+## [EXTRA] Create polygon BR to flag records inland ####
 
-# # Because 'sf_brazil' has each state border delimitation and I need only the
-# # country delimitation, it is easier to retrieve the polygon from {rnaturalearth}
-# 
-# sf_brazil_union <- rnaturalearth::ne_countries(country = "brazil",
-#                                                scale = 10,
-#                                                returnclass = "sf")
-# # mapview::mapview(sf_brazil_union)
-# 
-# ## Break it down and simplify a little bit
-# sf_brazil_union_line <- 
-#   sf_brazil_union %>% 
-#   sf::st_cast('MULTILINESTRING') %>% 
-#   sf::st_cast('LINESTRING', do_split = TRUE) %>%
-#   dplyr::mutate(npts = mapview::npts(geometry, by_feature = TRUE)) %>% # sf::st_cast('POLYGON') %>% 
-#   dplyr::filter(npts > 40) %>% 
-#   sf::st_union()
-# # mapview::mapview(sf_brazil_union_line)
-# 
-# ## Create a 15km buffer
-# sf_brazil_buff <- sf::st_buffer(sf_brazil_union_line, dist = 15000)
-# # mapview::mapview(sf_brazil_buff)
-# 
-# sf_brazil_to_crop_terrestrial <-
-#   sf::st_difference(sf_brazil_union, sf_brazil_buff) %>% 
-#   dplyr::select(name_en, geometry)
-# # mapview::mapview(sf_brazil_to_crop_terrestrial)
-# 
-# ## Backup this polygon
-# # sf::st_write(sf_brazil_to_crop_terrestrial,
-# #              "./data-spatial/polygon-to-crop-terrestrial-records.gpkg")
-# 
-# rm(sf_brazil_union, sf_brazil_union_line, sf_brazil_buff)
+library(rnaturalearth)
 
-## [CHECK WITH COORDINATORS] Crop records within continental areas ####
+# Because 'sf_brazil' has each state border delimitation and I need only the
+# country delimitation, it is easier to retrieve the polygon from {rnaturalearth}
 
-# sfs_list <- list(
-#   spp_turtles = sf_turtles_spp,
-#   spp_seabirds = sf_birds_spp,
-#   spp_mammals = sf_mammals_spp
-# )
-# 
-# sfs_cropped_list <- list()
-# 
-# for (sf in 1:length(sfs_list)) {
-#   
-#   ## Get 'sf' name
-#   sf_name <- names(sfs_list)[sf]
-#   
-#   ## Get data according to 'sf' name
-#   tmp <- sfs_list[[sf_name]]
-#   
-#   ## Crop to 
-#   sf_cropped <-
-#     sf::st_difference(tmp, sf_brazil_to_crop_terrestrial) 
-#   
-#   ## Did not work, not sure why, but thats OK... 
-#   # sf_use_s2(FALSE) 
-#   # sf_cropped <- sf_cropped[sf::st_contains(sf_cropped, sf::st_make_valid(sf_blue_amazon)),]
-#   
-#   ## Rename objs
-#   sf_name_cropped <- paste0(sf_name, "_cropped")
-#   
-#   ## Store them in the list
-#   sfs_cropped_list[[sf_name_cropped]] <- sf_cropped
-#   
-#   rm(sf, sf_name, tmp, sf_name_cropped, sf_cropped)
-# }
+sf_brazil_union <- rnaturalearth::ne_countries(country = "brazil",
+                                               scale = 10,
+                                               returnclass = "sf")
+# mapview::mapview(sf_brazil_union)
 
-## [CHECK WITH COORDINATORS] In case we crop land records... save it ####
+## Break it down and simplify it a little bit
+sf_brazil_union_line <-
+  sf_brazil_union %>%
+  sf::st_cast('MULTILINESTRING') %>%
+  sf::st_cast('LINESTRING', do_split = TRUE) %>%
+  dplyr::mutate(npts = mapview::npts(geometry, by_feature = TRUE)) %>% # sf::st_cast('POLYGON') %>%
+  dplyr::filter(npts > 40) %>%
+  sf::st_union()
+# mapview::mapview(sf_brazil_union_line)
 
-# names(sfs_cropped_list)
-#
-# write.csv((sfs_cropped_list[["spp_turtles_cropped"]] %>% sf::st_drop_geometry()),
-#           file = "./data-processed/spp_sea-turtles_meow.csv")
-# 
-# write.csv((sfs_cropped_list[["spp_seabirds_cropped"]] %>% sf::st_drop_geometry()),
-#           file = "./data-processed/spp_seabirds_meow.csv")
-# 
-# write.csv((sfs_cropped_list[["spp_mammals_cropped"]] %>% sf::st_drop_geometry()),
-#           file = "./data-processed/spp_marine-mammals_meow.csv")
+## Create a 15km buffer
+sf_brazil_buff <- sf::st_buffer(sf_brazil_union_line, dist = 15000)
+# mapview::mapview(sf_brazil_buff)
+
+sf_brazil_to_crop_terrestrial <-
+  sf::st_difference(sf_brazil_union, sf_brazil_buff) %>%
+  dplyr::select(name_en, geometry)
+# mapview::mapview(sf_brazil_to_crop_terrestrial)
+
+## Backup this polygon
+# sf::st_write(sf_brazil_to_crop_terrestrial,
+#              "./data-spatial/polygon-to-crop-terrestrial-records.gpkg")
+
+rm(sf_brazil_union, sf_brazil_union_line, sf_brazil_buff)
